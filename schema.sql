cd9a26579842ba699c4c81058252fc4dd552bc20
tboyd4 <tboyd1898@gmail.com>
2020-03-03 17:58:21 -0600

    added create table code to the sql

---
+++ schema.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE meuixCMS_DB;
+
+USE meuixCMS_DB;
+
+CREATE TABLE department (
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
