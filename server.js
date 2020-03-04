dc545611602a5896346e454641b17f11cf051eb1
tboyd4 <tboyd1898@gmail.com>
2020-03-03 21:23:08 -0600

    organized code. Imports and SQL Connection done

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
-const mysql = require('mysql');
-const inquirer = require('inquirer');
-require('console.table');
-const keys = require('./config/keys/keys');
+// =================================== Imports and Require Statements ====================//
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+require("console.table");
+const keys = require("./config/keys/keys");
 
-
+// =================================== Setting Up mySQL Connection =======================//
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: keys.mysqlpass,
-    database: "meuixCMS_DB"
-  });
-  
-connection.connect(function(err) {
-    if (err) {
-        console.error("CONNECTION FAILURE : " + err.stack);
-        return;
-    }
-    console.log("CONNECTION SUCCESS.. Thread ID : " + connection.threadId);
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: keys.mysqlpass,
+  database: "meuixCMS_DB"
 });
 
-connection.query('SELECT * FROM employee', (err, res) => {
-    if (err) throw err;
-
-    console.log(res);
-})
+connection.connect(function(err) {
+  if (err) {
+    console.error("CONNECTION FAILURE : " + err.stack);
+    return;
+  }
+  console.log("CONNECTION SUCCESS.. Thread ID : " + connection.threadId);
+});
+// =======================================================================================//
